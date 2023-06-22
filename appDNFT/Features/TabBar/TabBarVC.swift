db9f568b2decdc26310b76ff5d81f9bcdb39c75a
devdanaragao <danaragaodev@gmail.com>
2023-06-22 23:36:50 +0100

    add controller wallet a TabBarVC.

--- appDNFT/Features/TabBar/TabBarVC.swift
+++ appDNFT/Features/TabBar/TabBarVC.swift
@@ -16,14 +16,17 @@ class TabBarVC: UITabBarController {
     
     private func setupTabBar() {
         let main = UINavigationController(rootViewController: MainVC())
-        setViewControllers([main], animated: false)
-        tabBar.isTranslucent = false
+        let wallet = UINavigationController(rootViewController: WalletVC())
+        setViewControllers([main,wallet], animated: false)
+        tabBar.isTranslucent = true
         tabBar.tintColor = .green
-//        tabBar.backgroundColor = .systemGray
+//        tabBar.backgroundColor = UIColor(red: 140/255, green: 12/255, blue: 216/255, alpha: 1)
+        tabBar.backgroundColor = .purple
         
         guard let items = tabBar.items else { return }
         
         items[0].image = UIImage(systemName: "person")
+        items[1].image = UIImage(systemName: "wallet.pass")
     }
 
 }
